e3d317f1084fb1bdbc3b489839cc6fbdb9d59f35
Golubkov Pavel <golubkov.p.d@gmail.com>
2016-08-16 20:00:47 +0500

    add configuration files

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+var tsify = require("tsify");
+
+gulp.task("default", function () {
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['js/app.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest("dist"));
+});
