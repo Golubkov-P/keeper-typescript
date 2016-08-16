5f20831104f619b500d4c5282d1da61c36bce70b
Golubkov Pavel <golubkov.p.d@gmail.com>
2016-08-16 20:18:34 +0500

    add app file and index.html

---
+++ js/app.ts
@@ -0,0 +1,28 @@
+import {Controller} from './Controller';
+import {Model} from './Model';
+import {Store} from './store';
+import {Template} from './template';
+import {View} from './View';
+
+class Keeper {
+	store: any;
+	model: any;
+	template: any;
+	view: any;
+	controller: any;
+	constructor() {
+		this.store = new Store('note-app-store');
+		this.model = new Model(this.store);
+		this.template = new Template();
+		this.view = new View(this.template);
+		this.controller = new Controller(this.model, this.view);
+	}
+}
+
+let KeeperApp = new Keeper();
+
+function loadView() {
+	KeeperApp.controller.emmitEvent();
+}
+
+document.addEventListener('load', loadView);
