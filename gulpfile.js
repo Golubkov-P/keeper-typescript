2feeafdddb8d16622ec82956083721ebdfed47fb
Golubkov Pavel <golubkov.p.d@gmail.com>
2016-08-16 20:15:51 +0500

    fix gulpfile.js and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-var gulp = require("gulp");
-var browserify = require("browserify");
-var source = require('vinyl-source-stream');
-var tsify = require("tsify");
+var gulp				= require("gulp");
+var browserify	= require("browserify");
+var source			= require('vinyl-source-stream');
+var tsify				= require("tsify");
 
 gulp.task("default", function () {
     return browserify({
